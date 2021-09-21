5b8498bfb2ebe0cf405fda16b6f8904ef8ad7496
svaldessamperio <svaldes_samperio@hotmail.com>
2021-09-21 15:22:05 -0500

    Línea Adicional

--- src/screens/SectionListScreen.tsx
+++ src/screens/SectionListScreen.tsx
@@ -46,6 +46,7 @@ export default function SectionListScreen() {
                     <HeaderTitle title={'Total: ' + section.data.length}/>     
                 )}
                 SectionSeparatorComponent={()=><ItemSeparator/>}
+                
             />
         </View>
     )
